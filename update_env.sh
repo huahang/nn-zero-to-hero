42321fb2f17ce0a213c2b62bb39333eabbb095f9
huahang <huahang.liu@gmail.com>
2024-12-08 11:12:32 +0000

    install pytorch-cuda for linux only
    Signed-off-by: huahang <huahang.liu@gmail.com>

--- update_env.sh
+++ update_env.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-conda env update --file environment.yml --prune
+conda env update -n micrograd --file environment.yml --prune
+
+if [ "$OS" = "Linux" ]; then
+  conda install -n micrograd pytorch-cuda=12.4 -c pytorch
+fi
